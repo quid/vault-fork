0ad99b0c64578c5ba3b9bc57d524cb93351655f6
Meggie <meggie@hashicorp.com>
2020-07-13 18:43:30 -0400

    Add RC note on downloads page (#9473)

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -16,6 +16,11 @@ export default function DownloadsPage({ downloadData }) {
         version={VERSION}
         downloads={downloadData}
         changelog={changelogUrl}
+        prerelease={{
+          type: 'release candidate',
+          name: 'v1.5.0',
+          version: '1.5.0-rc',
+        }}
       />
     </div>
   )
